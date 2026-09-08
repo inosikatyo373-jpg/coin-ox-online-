49c2533226d4cef0f83bac1ceeb35194c05f0ba9
inosikatyo373-jpg <inosikatyo373@gmail.com>
2026-09-08 11:41:25 +0900

    Bust cache for Jack idle motion across all full-body views

--- public/v38.js
+++ public/v38.js
@@ -1,9 +1,9 @@
-/* BID GRID v3.11.19 - named character roster */
+/* BID GRID v3.11.20 - named character roster */
 (function(){
   if(!document.querySelector('link[data-v381-fullbody]')){
     const link=document.createElement('link');
     link.rel='stylesheet';
-    link.href='/v381.css?v=31119';
+    link.href='/v381.css?v=31120';
     link.dataset.v381Fullbody='1';
     document.head.appendChild(link);
   }
